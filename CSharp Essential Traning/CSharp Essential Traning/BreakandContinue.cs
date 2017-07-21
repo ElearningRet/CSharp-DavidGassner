7209d670f3e2d72ac7e0edc8649b4fe25b4e9b7d
rgvcycling <garza.r.tx@gmail.com>
2017-07-21 16:46:29 -0500

    new classes sections and the start of the Calculator program

---
+++ CSharp Essential Traning/CSharp Essential Traning/BreakandContinue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEssentialTraning
+{
+    class BreakandContinue
+    {
+        enum Fruit { Apple, Orange, Fig };
+
+        static void Main(string[] arg)
+        {
+            var thisFruit = Fruit.Apple;
+            
+            switch(thisFruit)
+            {
+                case Fruit.Apple:
+                    Console.WriteLine("You chose Apple");
+                    break; // breaks jumps to the bottom of the switch()
+
+                case Fruit.Fig:
+                    Console.WriteLine("You chose Fig");
+                    break; // breaks jumps to the bottom of the switch()
+
+                case Fruit.Orange:
+                    Console.WriteLine("You chose Orange");
+                    break; // breaks jumps to the bottom of the switch()
+
+                default:
+                    Console.WriteLine("You chose something else");
+                    break;
+
+            }
+
+            Fruit[] groceries = { Fruit.Apple, Fruit.Apple, Fruit.Orange };
+
+            Console.WriteLine("\nGrocery list");
+            int apples = 0;
+            foreach (var item in groceries)
+            {
+                //if (item == Fruit.Orange)
+                //{
+                //    break;
+                //}
+                if (item == Fruit.Apple)
+                {
+                    apples++;
+                    if (apples>1)
+                    {
+                        break;      // kicks out of the foreach loop and ends loop
+                        continue;   // skips rest of code in loop and goes to the next itereation of the loop
+                                    // can only be used in loops 
+                    }
+                }
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("End of grocery list");
+        }
+    }
+}
---
+++ CSharp Essential Traning/CSharp Essential Traning/Calculator.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpEssentialTraning
+{
+    class Calculator
+    {
+        static void Main(string[] arg)
+        {
+            double value1 = 0;
+            double value2 = 0;
+            string operation;
+
+            Console.WriteLine("Enter value #1:");
+            value1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter value #2:");
+            value2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Choose your opprator -> (A)dd, (S)ubtract, (M)ultiply or (D)ivide");
+            operation = Convert.ToString(Console.Read());
+
+            switch (operation)
+            {
+                case A:
+                    Console.WriteLine("{0} + {1} = {2}", value1, value2, Addition(value1, value2));
+                    break;
+
+                case S:
+                    Console.WriteLine("{0} - {1} = {2}", value1, value2, Subtraction(value1, value2));
+                    break;
+
+
+                case M:
+                    Console.WriteLine("{0} * {1} = {2}",value1, value2, Multiplication(value1,value2));
+                    break;
+
+                case D:
+                    Console.WriteLine("{0} / {1} = {2}", value1, value2, Divide(value1, value2));
+                    break;
+
+
+                default:
+                    break;
+            }
+
+        }
+
+        static double Addition(double value1, double value2)
+        {
+            return value1 + value2;
+        }
+
+        static double Subtraction(double value1, double value2)
+        {
+            return value1 - value2;
+        }
+
+        static double Multiplication(double value1, double value2)
+        {
+            return value1 * value2;
+        }
+
+        static double Divide(double value1, double value2)
+        {
+            return value1 / value2;
+        }
+        
+    }
+}
